31968e8738d7709a13aef6d733cfe9bdec399e9d
Альберт <@HOME-PC>
2021-11-15 01:11:48 +0700

    ОбновлениеДобавьте файлы проекта.

---
+++ POEtrade/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿
+namespace POEtrade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.aBD41_18DataSet = new POEtrade.ABD41_18DataSet();
+            this.categoryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.categoryTableAdapter = new POEtrade.ABD41_18DataSetTableAdapters.CategoryTableAdapter();
+            this.rarityBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.rarityTableAdapter = new POEtrade.ABD41_18DataSetTableAdapters.RarityTableAdapter();
+            this.socketBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.socketTableAdapter = new POEtrade.ABD41_18DataSetTableAdapters.SocketTableAdapter();
+            this.colorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.colorTableAdapter = new POEtrade.ABD41_18DataSetTableAdapters.ColorTableAdapter();
+            this.linkBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.linkTableAdapter = new POEtrade.ABD41_18DataSetTableAdapters.LinkTableAdapter();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBD41_18DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rarityBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.socketBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.linkBindingSource)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(629, 157);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 176);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.categoryBindingSource;
+            this.comboBox1.DisplayMember = "category";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(153, 175);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.ValueMember = "id_category";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DataSource = this.rarityBindingSource;
+            this.comboBox2.DisplayMember = "rarity";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(280, 175);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.ValueMember = "id_rarity";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DataSource = this.socketBindingSource;
+            this.comboBox3.DisplayMember = "socket";
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(407, 175);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 5;
+            this.comboBox3.ValueMember = "id_socket";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DataSource = this.linkBindingSource;
+            this.comboBox4.DisplayMember = "link";
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(26, 202);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 8;
+            this.comboBox4.ValueMember = "id_link";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DataSource = this.colorBindingSource;
+            this.comboBox5.DisplayMember = "color";
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(534, 175);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 7;
+            this.comboBox5.ValueMember = "id_color";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(153, 203);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(121, 20);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(280, 203);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(121, 20);
+            this.numericUpDown2.TabIndex = 10;
+            // 
+            // aBD41_18DataSet
+            // 
+            this.aBD41_18DataSet.DataSetName = "ABD41_18DataSet";
+            this.aBD41_18DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // categoryBindingSource
+            // 
+            this.categoryBindingSource.DataMember = "Category";
+            this.categoryBindingSource.DataSource = this.aBD41_18DataSet;
+            // 
+            // categoryTableAdapter
+            // 
+            this.categoryTableAdapter.ClearBeforeFill = true;
+            // 
+            // rarityBindingSource
+            // 
+            this.rarityBindingSource.DataMember = "Rarity";
+            this.rarityBindingSource.DataSource = this.aBD41_18DataSet;
+            // 
+            // rarityTableAdapter
+            // 
+            this.rarityTableAdapter.ClearBeforeFill = true;
+            // 
+            // socketBindingSource
+            // 
+            this.socketBindingSource.DataMember = "Socket";
+            this.socketBindingSource.DataSource = this.aBD41_18DataSet;
+            // 
+            // socketTableAdapter
+            // 
+            this.socketTableAdapter.ClearBeforeFill = true;
+            // 
+            // colorBindingSource
+            // 
+            this.colorBindingSource.DataMember = "Color";
+            this.colorBindingSource.DataSource = this.aBD41_18DataSet;
+            // 
+            // colorTableAdapter
+            // 
+            this.colorTableAdapter.ClearBeforeFill = true;
+            // 
+            // linkBindingSource
+            // 
+            this.linkBindingSource.DataMember = "Link";
+            this.linkBindingSource.DataSource = this.aBD41_18DataSet;
+            // 
+            // linkTableAdapter
+            // 
+            this.linkTableAdapter.ClearBeforeFill = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(120, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Редактировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(227, 238);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(26, 277);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(135, 49);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Поиск";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.comboBox6);
+            this.groupBox2.Location = new System.Drawing.Point(183, 277);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(135, 77);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Фильтрация";
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.DataSource = this.categoryBindingSource;
+            this.comboBox6.DisplayMember = "category";
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(8, 18);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 16;
+            this.comboBox6.ValueMember = "id_category";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(8, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Фильтр";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 424);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox4);
+            this.Name = "Form1";
+            this.Text = "POE trade";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBD41_18DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.categoryBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rarityBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.socketBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.linkBindingSource)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private ABD41_18DataSet aBD41_18DataSet;
+        private System.Windows.Forms.BindingSource categoryBindingSource;
+        private ABD41_18DataSetTableAdapters.CategoryTableAdapter categoryTableAdapter;
+        private System.Windows.Forms.BindingSource rarityBindingSource;
+        private ABD41_18DataSetTableAdapters.RarityTableAdapter rarityTableAdapter;
+        private System.Windows.Forms.BindingSource socketBindingSource;
+        private ABD41_18DataSetTableAdapters.SocketTableAdapter socketTableAdapter;
+        private System.Windows.Forms.BindingSource colorBindingSource;
+        private ABD41_18DataSetTableAdapters.ColorTableAdapter colorTableAdapter;
+        private System.Windows.Forms.BindingSource linkBindingSource;
+        private ABD41_18DataSetTableAdapters.LinkTableAdapter linkTableAdapter;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox6;
+    }
+}
+
---
+++ POEtrade/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Linq;
+using System.Data.SqlClient;
+
+namespace POEtrade
+{
+    public partial class Form1 : Form
+    {
+        static string conStr = "Data Source=10.10.0.120;Initial Catalog=ABD41_18;User ID=abd31_21;Password=123456qwerty.";
+        DataContext context = new DataContext(conStr);
+        public Form1()
+        {
+            InitializeComponent();
+            Table<Item> item= context.GetTable<Item>();
+            dataGridView1.DataSource = item.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Item it = new Item
+            {
+                name = textBox1.Text,
+                category = Convert.ToInt32(comboBox1.SelectedValue),
+                rarity = Convert.ToInt32(comboBox2.SelectedValue),
+                socket = Convert.ToInt32(comboBox3.SelectedValue),
+                color = Convert.ToInt32(comboBox5.SelectedValue),
+                link = Convert.ToInt32(comboBox4.SelectedValue),
+                quantity = Convert.ToInt32(numericUpDown1.Value),
+                price = Convert.ToInt32(numericUpDown2.Value),
+            };
+            context.GetTable<Item>().InsertOnSubmit(it);
+            context.SubmitChanges();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "aBD41_18DataSet.Link". При необходимости она может быть перемещена или удалена.
+            this.linkTableAdapter.Fill(this.aBD41_18DataSet.Link);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "aBD41_18DataSet.Color". При необходимости она может быть перемещена или удалена.
+            this.colorTableAdapter.Fill(this.aBD41_18DataSet.Color);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "aBD41_18DataSet.Socket". При необходимости она может быть перемещена или удалена.
+            this.socketTableAdapter.Fill(this.aBD41_18DataSet.Socket);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "aBD41_18DataSet.Rarity". При необходимости она может быть перемещена или удалена.
+            this.rarityTableAdapter.Fill(this.aBD41_18DataSet.Rarity);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "aBD41_18DataSet.Category". При необходимости она может быть перемещена или удалена.
+            this.categoryTableAdapter.Fill(this.aBD41_18DataSet.Category);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Item it = context.GetTable<Item>().FirstOrDefault(x => x.id_item == Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value));
+            it.name = textBox1.Text;
+            it.category = Convert.ToInt32(comboBox1.SelectedValue);
+            it.rarity = Convert.ToInt32(comboBox2.SelectedValue);
+            it.socket = Convert.ToInt32(comboBox3.SelectedValue);
+            it.color = Convert.ToInt32(comboBox5.SelectedValue);
+            it.link = Convert.ToInt32(comboBox4.SelectedValue);
+            it.quantity = Convert.ToInt32(numericUpDown1.Value);
+            it.price = Convert.ToInt32(numericUpDown2.Value);
+            context.SubmitChanges();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Item item = context.GetTable<Item>().OrderByDescending(x => x.id_item).FirstOrDefault();
+            context.GetTable<Item>().DeleteOnSubmit(item);
+            context.SubmitChanges();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            var f = context.GetTable<Item>().Where(x => x.name.Contains(textBox2.Text));
+            dataGridView1.DataSource = f.ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var f = context.GetTable<Item>().Where(x => x.category == Convert.ToInt32(comboBox6.SelectedValue));
+            dataGridView1.DataSource = f.ToList();
+        }
+    }
+}
